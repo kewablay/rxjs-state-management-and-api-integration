3506a4b5955d06761159e83e1d97342a6164a63e
kewablay <kojokewajunior123@gmail.com>
2024-09-08 18:05:52 +0000

    added app component content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsListComponent, CartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
-export class AppComponent {
-  title = 'rxjs-state-management-and-api-integration';
-}
+export class AppComponent {}
